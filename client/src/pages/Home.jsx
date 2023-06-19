7c924b98bb9cc2268cec0dd07a0622cc3bf81d5e
kevinlindoames <kevinlindo@hotmail.com>
2023-06-18 22:07:42 -0500

    si

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -31,7 +31,7 @@ useEffect(() => {
 
     try {
 
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://dall-e-dm3u.onrender.com/api/v1/post', {
         method: 'GET' ,
         headers: {
           'Content-Type': 'application/json',
